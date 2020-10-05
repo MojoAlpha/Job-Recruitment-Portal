476bc59f63102f5cf0ad30a5f3787c1874a8c966
lovedeep <developer.lovedeep@gmail.com>
2020-10-06 00:33:43 +0530

    added custom css file
    also added basic properties like fonts

--- frontend/src/components/home/index.js
+++ frontend/src/components/home/index.js
@@ -3,7 +3,15 @@ import React from 'react'
 export default function Home() {
     return (
         <div>
-            Home component
+           <nav className="navbar fixed-top mx-4">
+                <a class="navbar-brand logo font-primary" href="/">Devhub</a>
+                <div className="nav-cta float-right">
+                    <button className="btn btn-outline-primary mr-2" >Log in</button>
+                    <button className="btn btn-primary" style={{color:"##11B0BB"}}>sign up</button>
+                </div>
+            </nav>
+
+            <div id="landing"></div>
         </div>
     )
 }
