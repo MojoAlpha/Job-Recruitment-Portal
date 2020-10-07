db2f6e48613aed90e7fd720a50a61e88fd155e8c
MojoAlpha <mohitp@mnnit.ac.in>
2020-10-07 15:40:40 +0530

    Company Authorization Routes Added. Verification Email Resend Route Added

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,178 +1,261 @@
 var User = require('../models/user')
+var Company = require('../models/company')
 const {validationResult} = require('express-validator')
 var crypto = require('crypto')
 var jwt = require('jsonwebtoken')
 var expressJwt = require('express-jwt')
 var keys = require('../config/keys');
-var mailer = require('../config/nodeMailer')
+var {emailVerification, welcomeEmail, forgetPassMail} = require('../config/mailTemplate')
 
-// USER AUTH CONTROLLERS
-
-exports.userSignup = (req, res) => {
+//SIGNUP CONTROLLER
+exports.Signup = (req, res) => {
     const errors = validationResult(req);
 
-    if(!errors.isEmpty()) {
-        return res.status(400).json({err: errors.array(), success: false})
-    }
-
-    User.findOne({email: req.body.email}, (err, user) => {
-        if(err) {
-            return res.status(500).json({err: err, success: false})
-        }
-        if(user) {
-            return res.status(401).json({err: 'Email Already Registered!!', success: false})
-        }
-
-        var newUser = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-            dp: 'dp/default.png',
-            verifyToken: crypto.randomBytes(30).toString('hex')
-        })
+    if(!errors.isEmpty())
+        return res.status(400).json({err: errors.array()[0].msg, success: false})
     
-        newUser.save((err, user) => {
-            if(err) {
-                return res.status(500).json({err: err, success: false})
-            }
-
-            console.log(user.email)
-            var mailOptions = {
-                to: user.email,
-                from: keys.EMAIL_ADDR,
-                subject: "Google Account Verification",
-                text:
-                    "Hello " + user.name.split(' ')[0] + " !!" +
-                    "\nTo proceed with the registration process, please click on the link below to verify your google account!!\n" +
-                    "Link : " + `http://localhost:8000/auth/verify/${user.verifyToken}` 
-            }
-    
-            mailer.sendMail(mailOptions, (err) => {
-                if(err) {
-                    return res.status(500).json({err: err, success: false})
-                }
-    
-                console.log("Confirmation Email Sent!!")
-            })
-
-            res.statusCode = 200
-            res.setHeader('Content-Type', 'application/json')
-            res.json({success: true, msg: "Successfully Registered!!"})
-        })
-    })
-}
-
-exports.userVerfication = (req, res) => {
-    User.findOne({verifyToken: req.params.token}, (err, user) => {
-        if(err || !user) {
-            return res.status(404).json({err: "Invalid Token!!", success: false})
-        }
-
-        user.isVerified = true
-        user.verifyToken = undefined
-        user.save((err, user) => {
-            if(err) {
-                return res.status(500).json({err: err, success: false})
-            }
+    User.findOne({ email: req.body.email }, (err, user) => {
+        Company.findOne({ email: req.body.email }, (err, company) => {            
+            if(err)
+                return res.status(500).json({ err: err, success: false})
             
-            var mailOptions = {
-                to: user.email,
-                from: keys.EMAIL_ADDR,
-                subject: "Welcome To DevHub",
-                text:
-                "Welcome " + user.name.split(' ')[0] + "!!\n" +
-                "We whole heartedly welcome you to the DevHub family. I hope you will make great connections through us and find your dream job!!\n" + 
-                "We kindly request you to like our Facebook page & follow us on instagram\n" +
-                "DevHub is a platform for professionals, to aid in your professional reach to people\n" +
-                "Wishing you a great day!!"
-            }
-    
-            mailer.sendMail(mailOptions, (err) => {
-                if(err) {
-                    return res.status(500).json({err: err, success: false})
-                }
-    
-                console.log("Welcome Email Sent!!")
-            })
-        })
+            if(user || company)
+                return res.status(403).json({ err: "Email Already Registered!!", success: false})
+            
+            if(req.body.type === "U") {
+                var newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password,
+                    verifyToken: crypto.randomBytes(30).toString('hex')
+                })
 
-        res.statusCode = 200
-        res.setHeader('Content-Type', 'application/json')
-        res.json({msg: "Verification Successfull!!", success: true})
+                newUser.save((err, user) => {
+                    if(err)
+                        return res.status(500).json({err: err, success: false})
+                    
+                    emailVerification(user.email, user.name, `http://localhost:8000/auth/verify/U/${user.verifyToken}`)
+                    res.statusCode = 200
+                    res.setHeader('Content-Type', 'application/json')
+                    res.json({success: true, msg: "User Successfully Registered!!"})
+                })
+            }
+            else if(req.body.type === "C") {
+                var newCompany = new Company({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password,
+                    verifyToken: crypto.randomBytes(30).toString('hex')
+                })
+
+                newCompany.save((err, company) => {
+                    if(err)
+                        return res.status(500).json({err: err, success: false})
+                    
+                    emailVerification(company.email, company.name, `http://localhost:8000/auth/verify/C/${company.verifyToken}`)
+                    res.statusCode = 200
+                    res.setHeader('Content-Type', 'application/json')
+                    res.json({success: true, msg: "Company Successfully Registered!!"})
+                })
+            }
+            else    
+            return res.status(403).json({err: "Invalid Signup!!", success: false}) 
+        })
     })
 }
 
-exports.userForgetVerify = (req, res) => {
+exports.Verfication = (req, res) => {
+    if(req.params.type === "U") {
+        User.findOne({verifyToken: req.params.token}, (err, user) => {
+            if(err || !user)
+                return res.status(404).json({err: "Invalid Token!!", success: false})
+    
+            user.isVerified = true
+            user.verifyToken = undefined
+            user.save((err, user) => {
+                if(err)
+                    return res.status(500).json({err: err, success: false})
+                
+                welcomeEmail(user.email, user.name)
+                res.statusCode = 200
+                res.setHeader('Content-Type', 'application/json')
+                res.json({ msg: "Verification Successfull!!", success: true, token: jwt.sign({_id: user._id, type: "U"}, keys.authKey, {expiresIn: 604800}) })  // 28 days validation
+            })
+        })
+    } 
+    else if(req.params.type === "C") {
+        Company.findOne({verifyToken: req.params.token}, (err, company) => {
+            if(err || !company)
+                return res.status(404).json({err: "Invalid Token!!", success: false})
+            
+            company.isVerified = true
+            company.verifyToken = undefined
+            company.save((err, company) => {
+                if(err)
+                    return res.status(500).json({err: err, success: false})
+                
+                welcomeEmail(company.email, company.name)
+                res.statusCode = 200
+                res.setHeader('Content-Type', 'application/json')
+                res.json({msg: "Verification Successfull!!", success: true, token: jwt.sign({_id: company._id, type: "C"}, keys.authKey, {expiresIn: 604800}) })
+            })
+        })
+    } else
+        return res.status(400).json({err: "Invalid Request!!", success: false})
+}
+
+exports.Login = (req, res) => {
+    if(req.body.email === undefined || req.body.password === undefined)
+        return res.status(400).json({ err: "Please Fill All The Fields!!", success: false})
+    
+    User.findOne({ email: req.body.email }, (err, user) => {
+        Company.findOne({ email: req.body.email }, (err, company) => {
+            if(err)
+                return res.status(500).json({ err: err, success: false })
+            
+            if(user) {
+                if(!user.authenticate(req.body.password)) {
+                    return res.status(401).json({err: "Email & Password Don't Match!!", success: false})
+                }
+
+                res.statusCode = 200
+                res.setHeader('Content-Type', 'application/json')
+                res.json({msg: 'Login Success As A Developer!!', success: true, token: jwt.sign({_id: user._id, type: "U"}, keys.authKey, {expiresIn: 604800})})
+            }
+            else if(company) {
+                if(!company.authenticate(req.body.password)) {
+                    return res.status(401).json({err: "Emaill & Password Don't Match!!", success: false})
+                }
+
+                res.statusCode = 200
+                res.setHeader('Content-Type', 'application/json')
+                res.json({msg: 'Login Success As A Company!!', success: true, token: jwt.sign({_id: company._id, type: "C"}, keys.authKey, {expiresIn: 604800})})
+            }
+        })
+    })
+}
+
+exports.ForgetVerify = (req, res) => {
+
     if(req.body.email === undefined) {
         return res.status(403).json({err: "Email isn't Provided!!", success: false})
     }
 
     User.findOne({ email: req.body.email }, (err, user) => {
-        if(err || !user) {
-            return res.status(404).json({err: "Email not registered", success: false})
-        }
+        Company.findOne({ email: req.body.email }, (err, company) => {
+            if(err)
+                return res.status(500).json({ err: err, success: false })
+            
+            if(user) {
+                user.verifyToken = crypto.randomBytes(30).toString('hex');
+                user.tokenExpiry = Date.now() + 3600000                     // verify token expires after 1 hour
+                user.save((err, user) => {
+                    if(err)
+                        return res.status(500).json({err: err, success: false})
 
-        user.verifyToken = crypto.randomBytes(30).toString('hex');
-        user.tokenExpiry = Date.now() + 3600000                     // verify token expires after 1 hour
-        user.save((err, user) => {
-            if(err) {
-                return res.status(500).json({err: err, success: false})
+                    forgetPassMail(user.email, `http://localhost:8000/auth/forget/U/${user.verifyToken}`)
+                    res.statusCode = 200
+                    res.setHeader('Content-Type', 'application/json')
+                    res.json({msg: "Mail Sent!!", success: true})
+                })
             }
-
-            var mailOptions = {
-                to: user.email,
-                from: keys.EMAIL_ADDR,
-                subject: "Password Reset Confirmation",
-                text:
-                    "This email has been sent in response to the request made to your account to reset your password!!\n" +
-                    "If it was you click on the link below, otherwise it will expire in next 15 minutes\n" + "Link : " +
-                    `http://localhost:8000/forget/verify/${user.verifyToken}`
+            else if(company) {
+                company.verifyToken = crypto.randomBytes(30).toString('hex');
+                company.tokenExpiry = Date.now() + 3600000
+                company.save((err, company) => {
+                    if(err)
+                        return res.status(500).json({err: err, success: false})
+                    
+                    forgetPassMail(company.email, `http://localhost:8000/auth/forget/C/${company.verifyToken}`)
+                    res.statusCode = 200
+                    res.setHeader('Content-Type', 'application/json')
+                    res.json({msg: "Mail Sent!!", success: true})
+                })
             }
-    
-            mailer.sendMail(mailOptions, (err) => {
-                if(err) {
-                    return res.status(500).json({err: err, success: false})
-                }
-    
-                console.log("Password Reset Mail Sent!!")
-            })
+            else
+                return res.status(404).json({ err: "Email Isn't Registered!!", success: false })
         })
-
-        res.statusCode = 200
-        res.setHeader('Content-Type', 'application/json')
-        res.json({msg: "Mail Sent!!", success: true})
     })
 }
 
-exports.userForgetReset = (req, res) => {
+exports.ForgetReset = (req, res) => {
     const errors = validationResult(req);
-
     if(!errors.isEmpty()) {
-        return res.status(400).json({err: errors.array(), success: false})
+        return res.status(400).json({err: errors.array()[0].msg, success: false})
     }
 
-    User.findOne({verifyToken: req.params.token}, (err, user) => {
-        if(err || !user) {
-            return res.status(400).json({err: "Invalid Token", success: false})
-        }
-
-        if(user.tokenExpiry < Date.now()) {
+    if(req.params.type === "U") {
+        User.findOne({verifyToken: req.params.token}, (err, user) => {
+            if(err || !user)
+                return res.status(404).json({err: "Invalid Token", success: false})
+    
+            if(user.tokenExpiry < Date.now()) {
+                user.verifyToken = undefined
+                user.tokenExpiry = undefined
+                user.save()
+                return res.status(403).json({err: "Token Expired!! Resend the request", success: false})
+            }
+    
             user.verifyToken = undefined
             user.tokenExpiry = undefined
-            user.save()
-            return res.status(403).json({err: "Token Expired!! Resend the request", success: false})
-        }
+            user.encry_password =  user.securePassword(req.body.newPassword)
+            user.save((err, user) => {
+                if(err)
+                    return res.status(500).json({err: err, success: false})
+            })
 
-        user.verifyToken = undefined
-        user.tokenExpiry = undefined
-        user.encry_password =  user.securePassword(req.body.newPassword)
-        user.save((err, user) => {
-            if(err) {
-                return res.status(500).json({err: err, success: false})
+            res.statusCode = 200
+            res.setHeader('Content-Type', 'application/json')
+            res.json({msg: "Password Reset Complete!!", success: true})
+        })
+    }
+    else if(req.params.type === "C") {
+        Company.findOne({verifyToken: req.params.token}, (err, company) => {
+            if(err || !company)
+                return res.status(404).json({err: "Invalid Token!!", success: false})
+            
+            if(Company.tokenExpiry < Date.now()) {
+                company.verifyToken = undefined
+                company.tokenExpiry = undefined
+                company.save()
+                return res.status(403).json({err: "Token Expired!! Resend the Request", success: false})
             }
 
-            var authToken = jwt.sign({_id: user._id, type: "U"}, keys.authKey, {expiresIn: 604800})
-            res.status(200).json({success: true, msg: "Password Updated!!", token: authToken})
+            company.verifyToken = undefined
+            company.tokenExpiry = undefined
+            company.encry_password = company.securePassword(req.body.newPassword)
+            company.save((err, company) => {
+                if(err)
+                    return res.status(500).json({err: err, success: false})
+            })
+
+            res.statusCode = 200
+            res.setHeader('Content-Type', 'application/json')
+            res.json({msg: "Password Reset Complete!!", success: true})
+        })
+    }
+    else
+        return res.status(400).json({err: "Invalid Request!!", success: false})
+}
+
+exports.resendVerify = (req, res) => {
+    if(req.body.email === undefined)
+        return res.status(400).json({ err: "Email Not Specified!!", success: false})
+    User.findOne({ email: req.body.email }, (err, user) => {
+        Company.findOne({ email: req.body.email }, (err, company) => {
+            if(err)
+                return res.status(500).json({err: err, success: false})
+            
+            if(user)
+                emailVerification(user.email, user.name, `http://localhost:8000/auth/verify/U/${user.verifyToken}`)
+            else if(company)
+                emailVerification(company.email, company.name, `http://localhost:8000/auth/verify/C/${company.verifyToken}`)
+            else
+                return res.status(404).json({ err: "Email Not Registered!!", success: false})
+            
+            res.statusCode = 200
+            res.setHeader('Content-Type', 'application/json')
+            res.json({success: true, msg: "Email Resend Complete!!"})
         })
     })
 }
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,42 +1,51 @@
 var express = require('express');
 var bodyParser = require('body-parser')
 const { body } = require('express-validator')
-var { userSignup, userVerfication, userForgetVerify, userForgetReset } = require('../controllers/auth');
+var { Signup, Login, Verfication, ForgetVerify, ForgetReset, resendVerify } = require('../controllers/auth');
 
 var authRouter = express.Router();
 authRouter.use(bodyParser.json());
 
 // USER AUTH ROUTES
+// type :- "U" - Normal User
+//         "C" - Company User
 
 /*  POST Route :- User SignUp
-    Req Body :- {email, name, password}
+    Req Body :- {email, name, password, type}
     Res Body :- {msg: "...", success: true, email} , if Sucessful Registered
                 {err: "...", success: false} , if Any Error Occurs */
-authRouter.post('/user/signup', [
-    body('email').isEmail().withMessage('Invalid Email'),
-    body('password').isLength({min: 8}).withMessage('Password should be greater than equal to 8')
-                    .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d@$.!%*#?&]/).withMessage('Password must contain alphabets, numbers & symbols')
-], userSignup)      // Checks on Password & Email of the user
+authRouter.post('/signup', [
+    body('type').notEmpty().withMessage('Invalid Signup Request. Specify The Type!!'),
+    body('name').notEmpty().withMessage("Name Shouldn't Be Kept Empty!!"),
+    body('email').isEmail().withMessage('Invalid Email!!'),
+    body('password').isLength({min: 8, max: 20}).withMessage('Password should be greater than equal to 8!!')
+                    .matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$.!%*#?&])[A-Za-z\d@$.!%*#?&]{8,}$/)
+                    .withMessage('Password must contain alphabets, numbers & symbols!!')
+], Signup)      // Checks on Password & Email of the user
 
 /*  PUT Route :- Verify User After SignUp
-    Req Body :- {email}
     Res Body :- {msg: "...", success: true} , if Sucessfully Verfied
                 {err: "...", success: false} , if Any Error Occurs */
-authRouter.put('/user/verify/:token', userVerfication)
+authRouter.put('/verify/:type/:token', Verfication)
+
+authRouter.post('/login', Login)
 
 /*  PUT Route :- Forget Password Email Verificaiton
     Req Body :- {email}
     Res Body :- {msg: "...", success: true} , if Sucessfully Sent
                 {err: "...", success: false} , if Any Error Occurs */
-authRouter.put('/user/forget', userForgetVerify)
+authRouter.put('/forget', ForgetVerify)
 
 /*  PUT Route :- Resetting The Password
     Req Body :- {newPassword}
     Res Body :- {msg: "...", success: true, token: authToken} , if Sucessfully Reset
                 {err: "...", success: false} , if Any Error Occurs */
-authRouter.put('/user/forget/:token', [
+authRouter.put('/forget/:type/:token', [
     body('newPassword').isLength({min: 8}).withMessage('Password should be greater than equal to 8')
-                       .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d@$.!%*#?&]/).withMessage('Password must contain alphabets, numbers & symbols')
-], userForgetReset)
+                       .matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$.!%*#?&])[A-Za-z\d@$.!%*#?&]{8,}$/)
+                       .withMessage('Password must contain alphabets, numbers & symbols')
+], ForgetReset)
+
+authRouter.get('/verify/resend', resendVerify)
 
 module.exports = authRouter;
