e9d34e7f2158a30edf162c597470ca577719ffb1
lovedeep <developer.lovedeep@gmail.com>
2020-10-16 00:05:29 +0530

    quick fix of frontend work due to backend change
    before the addition of new backend already i was having some local branches with incomplete work.so
    i copied the file and work from those branches and directly added to this branch.

--- frontend/src/components/registeredUser/NewsFeed.js
+++ frontend/src/components/registeredUser/NewsFeed.js
@@ -4,23 +4,36 @@ import InfoItem from "./components/InfoItem";
 function NewsFeed() {
   return (
     <>
-      <div className="col-sm-12 col-lg-8 bg-dark pt-4">
+      <div className="col-sm-12 col-lg-8 pt-4">
         <Post />
         <Post />
       </div>
-      <div className="col-3 bg-dark pt-4 d-none d-lg-block" style={{ height: "",position:"fixed",top:'88px',height:'100%',right:'0'}}>
-        <div className="bg-white p-4 rounded text-capitalize mb-5">
+      <div
+        className="col-3 pt-4 d-none d-lg-block"
+        style={{
+          height: "",
+          position: "fixed",
+          top: "88px",
+          height: "100%",
+          right: "0",
+        }}
+      >
+        <div className="bg-white p-4 rounded shadow text-capitalize mb-5">
           <p>here are some jobs based on your skills</p>
           <InfoItem />
           <InfoItem />
-          <a href="#" className="text-center d-block">see more</a>
+          <a href="#" className="text-center d-block">
+            see more
+          </a>
         </div>
 
-        <div className="bg-white p-4 rounded text-capitalize">
+        <div className="bg-white p-4 rounded shadow text-capitalize">
           <p>jobs that you have applied for</p>
-          <InfoItem/>
-          <InfoItem/>
-          <a href="#" className="d-block text-center text-underline">see more</a>
+          <InfoItem />
+          <InfoItem />
+          <a href="#" className="d-block text-center text-underline">
+            see more
+          </a>
         </div>
       </div>
     </>
--- frontend/src/components/registeredUser/index.js
+++ frontend/src/components/registeredUser/index.js
@@ -1,52 +1,127 @@
-import React,{useState}from "react";
-import NewsFeed from './NewsFeed'
-import GridLayout from './GridLayout'
+import React, { useState } from "react";
+import NewsFeed from "./NewsFeed";
+import MyNetwork from "./MyNetwork";
+// import Message from "./Message";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-
-const navLinkActive = "d-flex text-decoration-none justify-content-center align-items-center p-2 px-lg-2 py-lg-3 border border-primary rounded bg-primary text-white"
-const navLink = "d-flex text-decoration-none justify-content-center align-items-center p-2 px-lg-2 py-lg-3"
+const navLinkActive =
+  "d-flex text-decoration-none justify-content-center align-items-center p-2 px-lg-2 py-lg-3 border border-primary rounded bg-primary text-white";
+const navLink =
+  "d-flex text-decoration-none justify-content-center align-items-center p-2 px-lg-2 py-lg-3";
 export default function RegisteredUser() {
-    const [activeTab,setActiveTab] = useState(0);
-    return (
-        <React.Fragment>
-            <div className="container-fluid sticky-top align-items-center p-3 px-md-4  border-bottom shadow-sm bg-primary">
-                <div className="row align-items-center">
+  const [activeTab, setActiveTab] = useState(0);
+  return (
+    <React.Fragment>
+      <div className="container-fluid sticky-top align-items-center p-3 px-md-4  border-bottom shadow-sm bg-primary">
+        <div className="row align-items-center">
+          <h5 className=" col-md-3 text-white text-center font-weight-bold text-uppercase align-middle">
+            devhub
+          </h5>
 
-                        <h5 className=" col-md-3 text-white text-center font-weight-bold text-uppercase align-middle">devhub</h5>
-                    
-                    <div className="col-md-6">
-                        <form class="form mt-2 mt-md-0">
-                            <input class="form-control mr-sm-2" type="text" placeholder="Search for jobs, company, skills, person" aria-label="Search" />
-                        </form>
-                    </div>
-                    <div className="col-md-3">
-                    <a class="btn btn-outline-light" href="#">Sign up</a>
-                    </div>
-                </div>
-
+          <div className="col-md-6">
+            <form class="form mt-2 mt-md-0">
+              <input
+                class="form-control mr-sm-2"
+                type="text"
+                placeholder="Search for jobs, company, skills, person"
+                aria-label="Search"
+              />
+            </form>
+          </div>
+          <div className="col-md-3">
+            <a class="btn btn-outline-light" href="#">
+              Sign up
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="container-fluid d-flex no-gutters p-0">
+        <div
+          className="d-none d-md-block col-3 px-2 py-4 px-lg-4 px-xl-5 text-center bg-white shadow border-right"
+          style={{ position: "fixed", top: "88px", left: 0, height: "100%" }}
+        >
+          <div className="d-flex flex-column " style={{ height: "90vh" }}>
+            <a
+              href=""
+              className="d-flex text-decoration-none justify-content-center align-items-center p-2 px-lg-2 py-lg-3 py-xl-4 border border-primary rounded-pill bg-primary text-white font-weight-bold h6"
+            >
+              <span className="mr-4">
+                <i class="fas fa-plus"></i>
+              </span>
+              create new post
+            </a>
+            <div className="d-flex flex-column text-left  justify-content-around flex-grow-1">
+              <a
+                href="#"
+                className={activeTab == 0 ? navLinkActive : navLink}
+                onClick={() => setActiveTab(0)}
+              >
+                <span className="mr-4">
+                  <i class="fas fa-home"></i>
+                </span>
+                home
+              </a>
+              <a
+                href="#"
+                className={activeTab == 1 ? navLinkActive : navLink}
+                onClick={() => setActiveTab(1)}
+              >
+                <span className="mr-4">
+                  <i class="fas fa-user-friends"></i>
+                </span>
+                my network
+              </a>
+              <a
+                href="#"
+                className={activeTab == 2 ? navLinkActive : navLink}
+                onClick={() => setActiveTab(2)}
+              >
+                <span className="mr-4">
+                  <i class="far fa-comment-dots"></i>
+                </span>
+                messages
+              </a>
+              <a
+                href="#"
+                className={activeTab == 3 ? navLinkActive : navLink}
+                onClick={() => setActiveTab(3)}
+              >
+                <span className="mr-4">
+                  <i class="fas fa-briefcase"></i>
+                </span>
+                jobs
+              </a>
+              <a
+                href="#"
+                className={activeTab == 4 ? navLinkActive : navLink}
+                onClick={() => setActiveTab(4)}
+              >
+                <span className="mr-4">
+                  <i class="fas fa-bookmark"></i>
+                </span>
+                bla bla
+              </a>
             </div>
-            <div className="container-fluid d-flex no-gutters p-0" >
-                <div className="d-none d-md-block col-3 px-2 py-4 px-lg-4 px-xl-5 text-center bg-white shadow border-right" style={{position:'fixed',top:'88px',left:0,height:'100%'}}>
-                    <div className="d-flex flex-column " style={{height:'90vh'}}>
-                        <a href="" className="d-flex text-decoration-none justify-content-center align-items-center p-2 px-lg-2 py-lg-3 py-xl-4 border border-primary rounded-pill bg-primary text-white font-weight-bold h6">
-                            <span className="mr-4"><i class="fas fa-plus"></i></span>create new post
-                            </a>
-                        <div className="d-flex flex-column text-left  justify-content-around flex-grow-1">
-                        <a href="#" className={activeTab==0?navLinkActive:navLink} onClick={() => setActiveTab(0)}><span className="mr-4"><i class="fas fa-home"></i></span>home</a>
-                        <a href="#" className={activeTab==1?navLinkActive:navLink} onClick={() => setActiveTab(1)}><span className="mr-4"><i class="fas fa-user-friends"></i></span>my network</a>
-                        <a href="#" className={activeTab==2?navLinkActive:navLink} onClick={() => setActiveTab(2)}><span className="mr-4"><i class="far fa-comment-dots"></i></span>messages</a>
-                        <a href="#" className={activeTab==3?navLinkActive:navLink} onClick={() => setActiveTab(3)}><span className="mr-4"><i class="fas fa-briefcase"></i></span>jobs</a>
-                        <a href="#" className={activeTab==4?navLinkActive:navLink} onClick={() => setActiveTab(4)}><span className="mr-4"><i class="fas fa-bookmark"></i></span>bla bla</a>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-12 col-md-9 row bg-gray px-4 pt-4" style={{marginLeft:'25%'}}>
-                    <NewsFeed/>
-                    {/* <GridLayout/> */}
-                    {/* imported content here */}
-                </div>
-            </div>
-
-        </React.Fragment>
-    );
+          </div>
+        </div>
+        <div
+          className="col-12 col-md-9 row bg-gray px-4 pt-4"
+          style={{ marginLeft: "25%" }}
+        >
+          <BrowserRouter>
+            <Switch>
+              <Route path="/feed" component={NewsFeed} />
+              {/* <Route path="/search" component={Home} /> */}
+              <Route path="/mynetwork" component={MyNetwork} />
+              {/* <Route path="/message" exact component={Message} /> */}
+              {/* <Route path="/user/:username" exact component={RegisteredUser} /> */}
+            </Switch>
+          </BrowserRouter>
+          <NewsFeed />
+          {/* <GridLayout/> */}
+          {/* imported content here */}
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
